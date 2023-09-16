655fad1a78208f2a99dbaea5fda573484812c7b0
naveen <stupiddint@gmail.com>
2023-09-16 21:36:52 +0530

    modified

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -1,3 +1,4 @@
+
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { user, ChachaChaudhary } from 'assets/images';
@@ -16,14 +17,15 @@ const CustomTable = ({ children, ...props }) => {
 const ChatMessage = ({ message }) =>
 	message.role === 'user' ? (
 		<div className='flex items-end justify-end'>
-			<img className='w-10 h-10 rounded-full border-gray-400 mr-2' src={user} alt='User Image' />
 			<div className='bg-gray-300 border-gray-100 border-2 rounded-lg p-2 max-w-xl'>
 				<p>{message.content}</p>
 			</div>
+			<img className='w-10 h-10 rounded-full border-gray-400 mr-2' src={user} alt='User Image' />
+
 		</div>
 	) : (
 		<div className='flex items-end'>
-			<img className='w-20 h-20 rounded-full border-gray-400 mr-2' src={ChachaChaudhary} alt='Chacha Chaudhary' />
+			{/* <img className='w-20 h-20 rounded-full border-gray-400 mr-2' src={ChachaChaudhary} alt='Chacha Chaudhary' /> */}
 			<div className='bg-gray-100 border-gray-300 border-2 rounded-lg p-2 mr-20 max-w-3xl'>
 				<ReactMarkdown
 					children={message.content}
