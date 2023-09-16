6fbc8fadfa7989b4be8072ba7cd7b73bfc891f26
naveen <stupiddint@gmail.com>
2023-09-17 05:11:35 +0530

    code modularized

--- bot/chat-bot-api/models/bot_response.js
+++ bot/chat-bot-api/models/bot_response.js
@@ -1,2 +1,22 @@
 import express from 'express'
+import axios from 'axios';
 
+const bot_url = process.env.BOT_BASEURL;
+
+const bot = (promptInput) => {
+    const apiUrl = `${bot_url}?prompt_input=${encodeURIComponent(promptInput)}`;
+    const result = axios.get(apiUrl)
+        .then((response) => {
+            // Handle the response from FastAPI here
+            // console.log('FastAPI Response:', response.data);
+            return response.data
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            return error
+        });
+
+    return result
+}
+
+export default bot;
