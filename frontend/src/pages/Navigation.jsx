fdd29c3ad9183ca593a0f31fc418f2ab9219eb8a
Dipanshu Mahato <dipanshumahato@gmail.com>
2023-09-17 16:55:58 +0530

    api(without ui) for Navigation and nearby places
    Signed-off-by: Dipanshu Mahato <dipanshumahato@gmail.com>

--- frontend/src/pages/Navigation.jsx
+++ frontend/src/pages/Navigation.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+import 'react-spring-bottom-sheet/dist/style.css'
 
 export default function Navigation() {
     const [Latitude, setLatitude] = useState(0);
     const [Longitude, setLongitude] = useState(0);
+    const [counter, setCounter] = useState(0);
+    const [Reponse, setReponse] = useState({});
+
+    useEffect(() => {
+        axios.post('https://53ab-115-242-248-226.ngrok-free.app/updateLocation', {
+            "lat": Latitude,
+            "lon": Longitude
+        }).then(response => {
+            setReponse(response.data);
+        })
+    }, [Latitude, Longitude]);
+
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error)
+        navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true });
+        setTimeout(() => {
+            navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true });
+            setCounter(counter + 1);
+
+        }, 5000);
+
+
     } else {
         console.log("Geolocation not supported")
     }
@@ -18,12 +40,17 @@ export default function Navigation() {
     }
 
     function error() {
+        setLatitude(-1);
+        setLongitude(-1);
         console.log("Unable to retrieve your location");
     }
+
     return (
-        <div>{
-            `Latitude: ${Latitude}, Longitude: ${Longitude}`
-        }
+        <div>
+            <div>{`Latitude: ${Latitude}, Longitude: ${Longitude}\n`}</div>
+            <pre>{JSON.stringify(Reponse, null, 4)}</pre>
+            <div>{Reponse.type}</div>
         </div>
+
     )
 }
