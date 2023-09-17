db5ce29039aed79b918db06a7ce7b140c7faf00d
naveen <stupiddint@gmail.com>
2023-09-17 06:42:10 +0530

    Feat: english voice added

--- bot/chat-bot-api/controller/controller.js
+++ bot/chat-bot-api/controller/controller.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bot from '../models/bot_response.js';
+import bhashini from 'bhashini-translation';
 
 export async function test(req, res) {
     res.json('hello there!')
@@ -7,24 +8,55 @@ export async function test(req, res) {
 
 // Text in english
 export async function english_text_res(req, res) {
-    console.log('iside test func')
-    const prompt = req.body.prompt;
-    // const result = await bot(prompt)
-    return prompt;
+    try {
+        const prompt = req.body.prompt;
+        const result = await bot(prompt);
+
+        res.json({ result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 // return text and audio in english
 export async function english_text_audio_res(req, res) {
-    const prompt = req.body;
+    try {
+        const prompt = req.body.prompt;
+        const response = await bot(prompt);
+        const audioUri = await bhashini.tts('en', prompt)
+        const result = { response, audioUri }
+
+        res.json({ result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 
 }
 
 // return text in other language
 export async function text_in_selected_lang(req, res) {
-    const prompt = req.body;
+    try {
+        const prompt = req.body.prompt;
+        const result = await bot(prompt);
+
+        res.json({ result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 // return text and audio in other language
 export async function text_audio_in_selected_lang(req, res) {
-    const prompt = req.body;
+    try {
+        const prompt = req.body.prompt;
+        const result = await bot(prompt);
+
+        res.json({ result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
