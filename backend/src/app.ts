d0cfcf4a68b29d4af32fabfcc358bf988976b8ad
Dipanshu Mahato <dipanshumahato@gmail.com>
2023-09-17 09:15:11 +0530

    setup mongodb
    Signed-off-by: Dipanshu Mahato <dipanshumahato@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,14 @@ dotenv.config();
 
 import express from 'express';
 
+import mongoDbConfig from './config/mongoDbConfig';
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 app.listen(port, async () => {
+  await mongoDbConfig();
   return console.log(`Express is listening at http://localhost:${port}`);
 });
