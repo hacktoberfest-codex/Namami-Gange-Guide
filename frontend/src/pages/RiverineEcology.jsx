23d6bf9935859fa34df1532b0153f539d6502aa7
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2023-10-05 20:56:34 +0530

    Update heading

--- frontend/src/pages/RiverineEcology.jsx
+++ frontend/src/pages/RiverineEcology.jsx
@@ -9,7 +9,7 @@ function RiverineEcology() {
       <NavBar />
       <div className="heading stylish-heading">
         <h1>
-        	Riverine Ecology page!!!
+		Dive into the Hidden World of Riverine Ecosystems
         </h1>
       </div>
       <div className="carousel border-2 border-green-500">
