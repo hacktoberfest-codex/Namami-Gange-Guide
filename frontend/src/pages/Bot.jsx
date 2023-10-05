1db4f2541cea92f45504fd147fe9514676b47c62
naveen <stupiddint@gmail.com>
2023-10-05 07:02:57 +0530

    updated

--- frontend/src/pages/Bot.jsx
+++ frontend/src/pages/Bot.jsx
@@ -1,19 +1,22 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import speaking from 'assets/chacha-cahaudhary/Lspeaking-bg.png';
 import ChatBot from './ChatBot';
+import { BotStateContext } from 'context/BotState';
 
 const Bot = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const { botState, setBotState } = useContext(BotStateContext)
 
 	const handleButton = () => {
 		console.log('clicked');
 		setIsOpen(prev => !prev);
 	};
-
+	// idle  , waiting
 	return (
 		<section className='flex flex-row items-end px-3 py-4 '>
 			<img className='w-[20%]' src={speaking} alt='Chacha Chaudhary' onClick={handleButton} />
 			<div className={`w-[90%] ${isOpen ? 'block' : 'hidden'}`}>
+				{/* if state === idle for few seconds close chatbot */}
 				<ChatBot />
 			</div>
 		</section>
