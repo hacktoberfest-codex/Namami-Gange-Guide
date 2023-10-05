c116cb7930059c6fe04cf4c8c36ff4343db523fb
naveen <stupiddint@gmail.com>
2023-10-05 07:03:21 +0530

    select lang

--- frontend/src/components/SelectLang.jsx
+++ frontend/src/components/SelectLang.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
 
+/**
+ * English (India) en-IN
+ *  ["India", "hi-IN"]
+ */
 const options = [
-	{ value: 'en', label: 'English' },
-	{ value: 'hi', label: 'Hindi' },
-	{ value: 'od', label: 'Odia' }
+	{ value: 'en-IN', label: 'English' },
+	{ value: 'hi-IN', label: 'Hindi' }
 ];
 
 const SelectLang = ({ setLang }) => {
-	const [selectedOption, setSelectedOption] = useState(null);
+	const [selectedOption, setSelectedOption] = useState(options[0]);
+
 	return (
 		<div>
 			<Select
 				defaultValue={selectedOption}
 				onChange={data => {
-					setSelectedOption(data);
-					setLang(data);
+					// console.log(data.value);
+					setSelectedOption(data.value);
+					setLang(data.value);
 				}}
+				menuPlacement='top'
 				options={options}
 				isSearchable={false}
 			/>
