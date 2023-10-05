49c4ca3322b8b48607f1d2402c8615aeb35952dc
Vivek Pathak <vivekpathak2083@gmail.com>
2023-10-05 08:22:45 +0530

    Home page bot section content update

--- frontend/src/pages/Bot.jsx
+++ frontend/src/pages/Bot.jsx
@@ -11,12 +11,17 @@ const Bot = () => {
 	};
 
 	return (
+		<>
+		<section className={` ${isOpen ? 'hidden' : 'block'} font-sans text-center text-lg bg-slate-50 m-5 p-3 rounded-full `} >
+				To know more about holy river Ganga about it's history, significance and beyond..! Try a converstaion with <div className='inline font-bold'>Chacha Chaudhary</div> by clicking on him.
+			</section>
 		<section className='flex flex-row items-end px-3 py-4 '>
-			<img className='w-[20%]' src={speaking} alt='Chacha Chaudhary' onClick={handleButton} />
+			<img className='w-[20%] cursor-pointer' src={speaking} alt='Chacha Chaudhary' onClick={handleButton} />
 			<div className={`w-[90%] ${isOpen ? 'block' : 'hidden'}`}>
 				<ChatBot />
 			</div>
 		</section>
+		</>
 	);
 };
 
